3658f9d853d57de44eaef889942733e6f88b8992
amitrkulkarni232@gmail.com <amitrkulkarni232@gmail.com>
2018-08-19 07:06:22 -0400

    Added the .py file

--- Projects/Hand Gesture Recognition/skin_detector.py
+++ Projects/Hand Gesture Recognition/skin_detector.py
@@ -2,8 +2,9 @@
 # responsible skin detection of the user
 
 import cv2
-class SkinDetector:
+import numpy as np
 
+class SkinDetector:
     def __init__(self):
         """
         The constructor for the SkinDetector class
@@ -35,8 +36,8 @@ class SkinDetector:
         # Get the frame height and frame shape
         frame_height, frame_width = input.shape[:2]
 
-        print("The frame height is ", frame_height)
-        print("the frame width is ", frame_width)
+        # print("The frame height is ", frame_height)
+        # print("the frame width is ", frame_width)
 
         rectangle_size = 20
 
@@ -63,3 +64,91 @@ class SkinDetector:
 
         # Sample Rectangle 2
         cv2.rectangle(input, (a1,b1), (a2, b2), rect_color)
+
+    def calibrate(self, input):
+        """
+        Function to perform calibration
+        Calculate the user's skin color through thresholding
+        """
+        frame_height, frame_width = input.shape[:2]
+
+        # Convert the image to a hsv color space
+        hsv_input = cv2.cvtColor(input, cv2.COLOR_BGR2HSV)
+
+        # ASSUMPTION -> We are cropping an image using
+        # Cropping hsv_input
+        # What are we cropping? What is our region of interest?
+        samp1_x = frame_width // 5
+        samp1_y = frame_height // 2
+        samp1_w = 20
+        samp1_h = 20
+
+        samp2_x = frame_width // 5
+        samp2_y = frame_height // 3
+        samp2_w = 20
+        samp2_h = 20
+
+        sample_1 = hsv_input[samp1_y:samp1_y+samp1_h, samp1_x:samp1_x+samp1_w]
+        sample_2 = hsv_input[samp2_y:samp2_y+samp2_h, samp2_x:samp2_x+samp2_w]
+
+        # Calculate the thresolds for these images
+        self.calculate_thresholds(sample_1, sample_2)
+
+        self.calibrated = True
+
+
+    def calculate_thresholds(self, sample1, sample2):
+        """
+        Function to actually cacluate the skin HSV thresholds
+        """
+        offset_low_thresh = 80
+        offset_high_thresh = 30
+
+        # cv2.mean(array) -> Calcuates the mean of array elements
+        hsv_mean_sample_1 = cv2.mean(sample1)
+
+        hsv_mean_sample_2 = cv2.mean(sample2)
+
+        # H Value
+        self.h_low_thresh = min(hsv_mean_sample_1[0], hsv_mean_sample_2[0]) - offset_low_thresh
+        self.h_high_thresh = max(hsv_mean_sample_1[0], hsv_mean_sample_2[0]) + offset_high_thresh
+
+        # S Value
+        self.s_low_thresh = min(hsv_mean_sample_1[1], hsv_mean_sample_2[1]) - offset_low_thresh
+        self.s_high_thresh = max(hsv_mean_sample_1[1], hsv_mean_sample_2[1]) + offset_high_thresh
+
+        self.v_low_thresh = min(hsv_mean_sample_1[2], hsv_mean_sample_2[2]) - offset_low_thresh
+        self.v_high_thresh = max(hsv_mean_sample_1[2], hsv_mean_sample_2[2]) + offset_high_thresh
+
+        # print("Done calculating thresholds")
+        # print(self.calibrated)
+
+    def get_skin_mask(self, input):
+        """
+        Function to get the skin mask
+        """
+        w, h, channels = input.shape
+        skin_mask = np.zeros((w, h), np.uint8)
+
+        if not self.calibrated:
+            # The user has performed any calibration
+            # Retrun a numpy array of zeroes
+            return skin_mask
+
+        hsv_input = cv2.cvtColor(input, cv2.COLOR_BGR2HSV)
+
+        # skin_mask = cv2.inRange(hsv_input,
+        #             (self.h_low_thresh, self.s_low_thresh, self.v_low_thresh),
+        #             (self.h_high_thresh, self.s_high_thresh, self.v_high_thresh))
+
+        skin_mask = cv2.inRange(hsv_input,
+                    np.array([self.h_low_thresh, self.s_low_thresh, self.v_low_thresh]),
+                    np.array([self.h_high_thresh, self.s_high_thresh, self.v_high_thresh]))
+
+        structuring_element = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+        skin_mask = cv2.morphologyEx(skin_mask, cv2.MORPH_OPEN, structuring_element)
+
+        kernel = np.ones((3,3),np.uint8)
+        skin_mask = cv2.dilate(skin_mask, kernel, 3)
+
+        return skin_mask
