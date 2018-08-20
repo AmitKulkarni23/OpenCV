c2496bb6293ca026b4abf36cbad859aa0fd06a87
amitrkulkarni232@gmail.com <amitrkulkarni232@gmail.com>
2018-08-20 06:03:53 -0400

    Lowe and Upper Bound for skin done

--- Projects/Hand Gesture Recognition/hand_gesture_skin_hsv.py
+++ Projects/Hand Gesture Recognition/hand_gesture_skin_hsv.py
@@ -8,6 +8,13 @@ import cv2
 import numpy as np
 
 
+rect_x = 100
+rect_y = 100
+rect_x2 = 300
+rect_y2 = 300
+
+# We will draw a blue rectangle on the frame at the above mentioned points
+rect_color = (255, 0, 0)
 
 
 def main_method():
@@ -25,7 +32,6 @@ def main_method():
         frame = cv2.flip(frame, 1)
 
         display_count(frame)
-
 
         key = cv2.waitKey(1)
 
@@ -46,4 +52,28 @@ def display_count(input_frame):
     Function that calculates ROI and displays teh count of the fingers
     @param input_frame: The input frame from the webcam
     """
-    
+    # Now we will extract the region of interest from the input frame
+    global rect_x, rect_y, rect_color, rect_width, rect_height
+
+    roi = input_frame[rect_x:rect_x2, rect_y:rect_y2]
+
+    # Draw the rectangle on teh input frame
+    cv2.rectangle(input_frame, (rect_x, rect_y), (rect_x2, rect_y2), rect_color, 0)
+
+    # Now convert the region of interest( which falls in teh blue rectangle, where
+    # the user places his hand into HSV colorspace)
+    # Why HSV - HSV works best for skin detection
+
+    input_hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+
+    # Define the range of the skin
+    # Reference link - https://tinyurl.com/y7gp8scb
+
+    # Use cv2.inRange to perform actual color detection
+    # Note: These values need to be changed, if the skin detection is not propers
+    lower_bound_skin = np.array([0,20,70], dtype=np.uint8)
+    upper_bound_skin = np.array([20,255,255], dtype=np.uint8)
+
+    # Use inRange to extract skin color
+
+    skin_mask = cv2.inRange(input_hsv, lower_bound_skin, upper_bound_skin)
